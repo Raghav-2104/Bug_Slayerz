40e039ae23184692d028e5c7c1d399aa97760134
Purtee13 <126791497+Purtee13@users.noreply.github.com>
2023-03-02 23:36:30 +0530

    Add files via upload

---
+++ Connection.php
@@ -0,0 +1,9 @@
+<?php
+       $conn=mysqli_connect("localhost","root","","presc"); 
+
+        if (mysqli_connect_errno()) 
+        {
+            echo"Cannot connect to database" ;
+        }
+        
+?>
